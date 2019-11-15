315f6a3788a6ed897a5e52436d4ad87f527608bf
stevehanphoto <steveyhan@sbcglobal.net>
2019-11-15 01:08:17 -0800

    When all events has been cancelled, display message that no event is available

--- src/components/eventlist.jsx
+++ src/components/eventlist.jsx
@@ -4,19 +4,23 @@ import "./eventlist.css"
 
 class EventList extends Component {
   render() {
-    const events = this.props.events.map(event => {
+    const events =
+      this.props.events.map(event => {
       return <Event 
           key={event.id} 
           event={event} 
           onDelete={this.props.onDelete} 
           onIncrement={this.props.onIncrement} 
-      />;
+      />
     });
+    const eventslist = (this.props.events.length > 0) ?
+        <ul className="events-container list-unstyled">{events}</ul> :
+        <h5 className="pt-2 pl-2">There is no event available.</h5>;
     return (
       <div className="eventslist-container">
         <h2>Events/Courses</h2>
         <hr />
-        <ul className="events-container list-unstyled">{events}</ul>
+        {eventslist}
       </div>
     );
   }
